d79747d135e421c93cdb26b8f44258442372b214
Antarctica000 <149984387+Antarctica000@users.noreply.github.com>
2024-12-28 20:52:09 +0800

    feat: add echo command (#72)
    * add echo command
    
    * support echo command
    
    * make format
    
    * update
    
    ---------
    
    Co-authored-by: marsevilspirit <marsevilspirit@gmail.com>

--- src/cmd_admin.cc
+++ src/cmd_admin.cc
@@ -161,6 +161,12 @@ bool PingCmd::DoInitial(PClient* client) { return true; }
 
 void PingCmd::DoCmd(PClient* client) { client->SetRes(CmdRes::kPong, "PONG"); }
 
+EchoCmd::EchoCmd(const std::string& name, int16_t arity) : BaseCmd(name, arity, kCmdFlagsFast, kAclCategoryFast) {}
+
+bool EchoCmd::DoInitial(PClient* client) { return true; }
+
+void EchoCmd::DoCmd(PClient* client) { client->AppendString(client->argv_[1]); }
+
 const std::string InfoCmd::kInfoSection = "info";
 const std::string InfoCmd::kAllSection = "all";
 const std::string InfoCmd::kServerSection = "server";
@@ -179,7 +185,7 @@ bool InfoCmd::DoInitial(PClient* client) {
     return true;
   }
 
-  std::string argv_ = client->argv_[1].data();
+  std::string argv_ = client->argv_[1];
   // convert section to lowercase
   std::transform(argv_.begin(), argv_.end(), argv_.begin(), [](unsigned char c) { return std::tolower(c); });
   if (argc == 2) {
