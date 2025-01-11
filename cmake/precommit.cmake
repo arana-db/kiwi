408e1b5ed346aa7467d79c3da54e136b14567d29
alexstocks <alexstocks@foxmail.com>
2025-01-11 23:45:41 +0800

    fix bugs

--- cmake/precommit.cmake
+++ cmake/precommit.cmake
@@ -8,12 +8,12 @@
 SET(HOOKS_SOURCE_DIR "${CMAKE_SOURCE_DIR}/etc/script")
 set(GIT_HOOKS_DIR "${CMAKE_SOURCE_DIR}/.git/hooks")
 
-# ensure pre-commit hook is installed
-ADD_CUSTOM_TARGET(install_git_hooks
-    COMMAND ${CMAKE_COMMAND} -E make_directory ${GIT_HOOKS_DIR}
-    COMMAND ${CMAKE_COMMAND} -E copy_if_different
-        ${HOOKS_SOURCE_DIR}/precommit.sh
-        ${GIT_HOOKS_DIR}/pre-commit
-    COMMAND ${CMAKE_COMMAND} -E chmod 755 ${GIT_HOOKS_DIR}/pre-commit
-    COMMENT "Installing git hooks..."
-)
+# # ensure pre-commit hook is installed
+# ADD_CUSTOM_TARGET(install_git_hooks
+#     COMMAND ${CMAKE_COMMAND} -E make_directory ${GIT_HOOKS_DIR}
+#     COMMAND ${CMAKE_COMMAND} -E copy_if_different
+#         ${HOOKS_SOURCE_DIR}/precommit.sh
+#         ${GIT_HOOKS_DIR}/pre-commit
+#     COMMAND ${CMAKE_COMMAND} -E chmod 755 ${GIT_HOOKS_DIR}/pre-commit
+#     COMMENT "Installing git hooks..."
+# )
