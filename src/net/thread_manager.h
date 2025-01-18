50a39293e0e143e1a05ebe5f4dcf73f4e3c7a8c4
Ph0m1 <2732706745@qq.com>
2025-01-18 22:46:30 +0800

    after clang-format

--- src/net/thread_manager.h
+++ src/net/thread_manager.h
@@ -158,7 +158,7 @@ template <typename T>
 requires HasSetFdFunction<T>
 void ThreadManager<T>::OnNetEventCreate(int fd, const std::shared_ptr<Connection> &conn) {
   if (!clientCount_.compare_exchange_strong(expected, expected + 1, std::memory_order_seq_cst,
-                                            std::memory_order_seq_cst) || 
+                                            std::memory_order_seq_cst) ||
       expected >= netOptions_.GetMaxClients()) {
     INFO("Max client connetions, refuse new connection fd: %d", fd);
     std::string response = "-ERR max clients reached\r\n";
