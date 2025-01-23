36b40c0627cb0f76f4e5eb1d6f114ef82af6f978
lqx <lqxlucky@qq.com>
2025-01-23 21:10:41 +0800

    format code

--- src/storage/tests/lock_mgr_test.cc
+++ src/storage/tests/lock_mgr_test.cc
@@ -3,8 +3,8 @@
 //  LICENSE file in the root directory of this source tree. An additional grant
 //  of patent rights can be found in the PATENTS file in the same directory.
 
-#include <thread>
 #include <gtest/gtest.h>
+#include <thread>
 
 #include "src/lock_mgr.h"
 #include "src/mutex_impl.h"
