b37f1eb3c448a44d164f76658ba586d84086ca41
FinnTew <finntew@foxmail.com>
2025-04-13 16:44:20 +0800

    fmt code

--- src/kstd/mod.rs
+++ src/kstd/mod.rs
@@ -12,7 +12,7 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
+pub mod defer;
 pub mod lock_mgr;
 pub mod slice;
 pub mod status;
-pub mod defer;
