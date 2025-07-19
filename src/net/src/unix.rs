be9d84014d80a0b7402758ec9c4a92c7bb02f408
问问神奇海螺 <guozhihaoemail@gmail.com>
2025-07-19 18:02:10 +0800

    fix: window cli  (#79)
    * fix: add unix cfg
    
    * fix: fix cargo clippy
    
    * fix: fix fmt
    
    * fix: modify
    
    * fix: modify
    
    * fix: modify

--- src/net/src/unix.rs
+++ src/net/src/unix.rs
@@ -12,34 +12,11 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
-use crate::handle::process_connection;
-use crate::{Client, ServerTrait, StreamTrait};
+use crate::ServerTrait;
 use async_trait::async_trait;
-use log::info;
 use std::error::Error;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio::net::{UnixListener, UnixStream};
 
-pub struct UnixStreamWrapper {
-    stream: UnixStream,
-}
-
-impl UnixStreamWrapper {
-    pub fn new(stream: UnixStream) -> Self {
-        Self { stream }
-    }
-}
-
-#[async_trait]
-impl StreamTrait for UnixStreamWrapper {
-    async fn read(&mut self, buf: &mut [u8]) -> Result<usize, std::io::Error> {
-        self.stream.read(buf).await
-    }
-    async fn write(&mut self, data: &[u8]) -> Result<usize, std::io::Error> {
-        self.stream.write(data).await
-    }
-}
-
+#[allow(dead_code)]
 pub struct UnixServer {
     path: String,
 }
@@ -51,21 +28,75 @@ impl UnixServer {
     }
 }
 
-#[async_trait]
-impl ServerTrait for UnixServer {
-    async fn start(&self) -> Result<(), Box<dyn Error>> {
-        let _ = std::fs::remove_file(&self.path);
-        let listener = UnixListener::bind(&self.path)?;
-        info!("Listening on Unix Socket: {}", self.path);
+#[cfg(unix)]
+mod unix_impl {
+    use super::*;
+    use crate::handle::process_connection;
+    use crate::{Client, StreamTrait};
+    use log::{error, info};
+    use tokio::io::{AsyncReadExt, AsyncWriteExt};
+    use tokio::net::{UnixListener, UnixStream};
 
-        loop {
-            let (socket, _) = listener.accept().await?;
+    pub struct UnixStreamWrapper {
+        stream: UnixStream,
+    }
 
-            let s = UnixStreamWrapper::new(socket);
+    impl UnixStreamWrapper {
+        pub fn new(stream: UnixStream) -> Self {
+            Self { stream }
+        }
+    }
 
-            let mut client = Client::new(Box::new(s));
+    #[async_trait]
+    impl StreamTrait for UnixStreamWrapper {
+        async fn read(&mut self, buf: &mut [u8]) -> Result<usize, std::io::Error> {
+            self.stream.read(buf).await
+        }
+        async fn write(&mut self, data: &[u8]) -> Result<usize, std::io::Error> {
+            self.stream.write(data).await
+        }
+    }
 
-            tokio::spawn(async move { process_connection(&mut client).await.unwrap() });
+    #[async_trait]
+    impl ServerTrait for UnixServer {
+        async fn start(&self) -> Result<(), Box<dyn Error>> {
+            if let Err(e) = std::fs::remove_file(&self.path) {
+                if e.kind() != std::io::ErrorKind::NotFound {
+                    return Err(e.into());
+                }
+            }
+
+            let listener = UnixListener::bind(&self.path)?;
+            info!("Listening on Unix Socket: {}", self.path);
+
+            loop {
+                match listener.accept().await {
+                    Ok((socket, _)) => {
+                        let s = UnixStreamWrapper::new(socket);
+                        let mut client = Client::new(Box::new(s));
+                        tokio::spawn(async move {
+                            if let Err(e) = process_connection(&mut client).await {
+                                error!("Connection processing failed: {e:?}");
+                            }
+                        });
+                    }
+                    Err(e) => {
+                        error!("Failed to accept connection: {e:?}");
+                    }
+                }
+            }
+        }
+    }
+}
+
+#[cfg(not(unix))]
+mod unix_impl {
+    use super::*;
+
+    #[async_trait]
+    impl ServerTrait for UnixServer {
+        async fn start(&self) -> Result<(), Box<dyn Error>> {
+            Err("Unix sockets are not supported on this platform".into())
         }
     }
 }
