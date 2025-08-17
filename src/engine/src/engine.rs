52b9592367f94bcf6ff9d764036e50299af95c0b
Li Yazhou <me.ssword@gmail.com>
2025-08-17 18:23:19 +0800

    fix headers

--- src/engine/src/engine.rs
+++ src/engine/src/engine.rs
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2024-present, arana-db Community.  All rights reserved.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 use rocksdb::{
     BoundColumnFamily, ColumnFamilyRef, DBIteratorWithThreadMode, Error, IteratorMode, ReadOptions,
     Snapshot, WriteBatch, WriteOptions, DB,
@@ -61,7 +80,7 @@ pub trait Engine: Send + Sync {
         writeopts: &WriteOptions,
     ) -> Result<()>;
 
-    // Iterator
+    // 迭代器
     fn iterator(&self, mode: IteratorMode) -> DBIteratorWithThreadMode<'_, DB>;
 
     fn iterator_opt(
