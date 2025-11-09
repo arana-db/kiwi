3d8e65dbc7ca2133781dd5c570590d2459b3227e
guozhihao <guozhihaoemail@gmail.com>
2025-11-10 01:58:40 +0800

    test: add test for is_stale

--- src/storage/src/lib.rs
+++ src/storage/src/lib.rs
@@ -20,13 +20,13 @@ mod member_data_key_format;
 
 mod base_filter;
 mod base_key_format;
-mod base_meta_value_format;
-mod base_value_format;
+pub mod base_meta_value_format;
+pub mod base_value_format;
 mod custom_comparator;
 
-mod strings_value_format;
+pub mod strings_value_format;
 
-mod list_meta_value_format;
+pub mod list_meta_value_format;
 mod lists_data_key_format;
 
 mod coding;
